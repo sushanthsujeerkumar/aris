1603eac1dca948f4c670333fd7182c523b9a66c2
sushanth <sushanthsujeerkumar@gmail.com>
2025-01-03 17:07:32 +0530

    joystick

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -25,8 +25,14 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
     )
+
+    # joystick = IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource([os.path.join(
+    #                 get_package_share_directory(package_name),'launch','joystick.launch.py'
+    #             )]), launch_arguments={'use_sim_time': 'true'}.items()
+    # )
 
     
 
@@ -92,6 +98,7 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
+        # joystick,
         delayed_controller_manager,
         delayed_diff_drive_spawner,
         delayed_joint_broad_spawner
